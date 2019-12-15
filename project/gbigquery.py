50e34316e9180a8c6bec7f83756944bf5c752920
deepakdeopura <Ohio*1234>
2019-12-14 18:38:14 -0800

    google bigquery sample connect and query

--- project/gbigquery.py
+++ project/gbigquery.py
@@ -1,11 +1,30 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
+
 credentials = service_account.Credentials.from_service_account_file('C:\Deepak\MS\BQ\cmddeopura-019c0c55e161.json')
 project_id = 'cmddeopura'
 client = bigquery.Client(credentials= credentials,project=project_id)
 
-query_txt="SELECT CONCAT( 'https://stackoverflow.com/questions/',       CAST(id as STRING)) as url,  view_count FROM `bigquery-public-data.stackoverflow.posts_questions` WHERE tags like '%google-bigquery%' ORDER BY view_count DESC LIMIT 10"
-query_job = client.query(query_txt)
-results = query_job.result()
-for row in results:
-    print("{} : {} views".format(row.url, row.view_count))
+def samlequery():
+    query_txt="SELECT CONCAT( 'https://stackoverflow.com/questions/',       CAST(id as STRING)) as url,  view_count FROM `bigquery-public-data.stackoverflow.posts_questions` WHERE tags like '%google-bigquery%' ORDER BY view_count DESC LIMIT 10"
+    query_job = client.query(query_txt)
+    results = query_job.result()
+    for row in results:
+        print("{} : {} views".format(row.url, row.view_count))
+
+def listdatasets():
+    datasets = list(client.list_datasets())
+    project = client.project
+    if datasets:
+        print("Datasets in project {}:".format(project))
+        for dataset in datasets:
+            print("\t{}".format(dataset.dataset_id))
+    else:
+        print("{} project does not contain any datasets.".format(project))
+
+def createdataset():
+    print("creating dataset")
+
+listdatasets()
+#samlequery()
+
